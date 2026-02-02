d4aed97ac8d42a7abaf9aaaaf342725af5efb9ac
Adam McNeilly <amcneilly331@gmail.com>
2026-02-01 20:06:28 -0500

    Adding RecipeListHeader component.  (#5)

---
+++ shared/src/commonMain/kotlin/com/mcloo/recipes/shared/RecipeListHeader.kt
@@ -0,0 +1,136 @@
+package com.mcloo.recipes.shared
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mcloo.recipes.shared.ui.theme.McLooTheme
+
+@Composable
+fun RecipeListHeader(
+    searchText: TextFieldValue,
+    onSearchTextChanged: (TextFieldValue) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Surface(
+        modifier = modifier,
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier
+                .padding(16.dp),
+        ) {
+            TitleAndAddButton()
+
+            SearchBar(
+                searchText = searchText,
+                onSearchTextChanged = onSearchTextChanged,
+                modifier = Modifier
+                    .fillMaxWidth(),
+            )
+        }
+    }
+}
+
+@Composable
+private fun SearchBar(
+    searchText: TextFieldValue,
+    onSearchTextChanged: (TextFieldValue) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    OutlinedTextField(
+        value = searchText,
+        onValueChange = onSearchTextChanged,
+        shape = RoundedCornerShape(8.dp),
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null,
+            )
+        },
+        placeholder = {
+            Text(
+                text = "Search Recipes...",
+            )
+        },
+        modifier = modifier,
+    )
+}
+
+@Composable
+private fun TitleAndAddButton() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Title(
+            modifier = Modifier
+                .weight(1F),
+        )
+
+        AddButton(
+            onClick = {
+            },
+        )
+    }
+}
+
+@Composable
+private fun AddButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    FloatingActionButton(
+        onClick = onClick,
+        shape = CircleShape,
+        modifier = modifier
+            .size(48.dp),
+    ) {
+        Icon(
+            imageVector = Icons.Default.Add,
+            contentDescription = null,
+        )
+    }
+}
+
+@Composable
+private fun Title(
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = "McLoo Recipes",
+        style = MaterialTheme.typography.titleLarge,
+        modifier = modifier,
+    )
+}
+
+@Preview
+@Composable
+fun RecipeListHeaderPreview() {
+    McLooTheme {
+        RecipeListHeader(
+            searchText = TextFieldValue(),
+            onSearchTextChanged = {},
+            modifier = Modifier
+                .fillMaxWidth(),
+        )
+    }
+}
