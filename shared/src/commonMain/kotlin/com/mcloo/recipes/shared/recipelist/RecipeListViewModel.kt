83aa1a5b1fd8710ecb032620962e37cab4353a82
Adam McNeilly <amcneilly331@gmail.com>
2026-02-17 20:30:50 -0500

    Observing searchText to pull recipes.

--- shared/src/commonMain/kotlin/com/mcloo/recipes/shared/recipelist/RecipeListViewModel.kt
+++ shared/src/commonMain/kotlin/com/mcloo/recipes/shared/recipelist/RecipeListViewModel.kt
@@ -2,14 +2,41 @@ package com.mcloo.recipes.shared.recipelist
 
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mcloo.recipes.shared.data.repositories.RecipeRepository
+import com.mcloo.recipes.shared.displaymodels.RecipeDisplayModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
 
-class RecipeListViewModel : ViewModel() {
+class RecipeListViewModel(
+    private val repository: RecipeRepository,
+) : ViewModel() {
     private val mutableState = MutableStateFlow(RecipeListUiState.default())
 
     val state = mutableState.asStateFlow()
+
+    init {
+        state
+            .map { state ->
+                state.searchText
+            }.distinctUntilChanged()
+            .onEach { searchText ->
+                repository.getRecipes(searchText.text).collect { recipes ->
+                    val displayModels = recipes.map(::RecipeDisplayModel)
+
+                    mutableState.update { currentState ->
+                        currentState.copy(
+                            recipes = displayModels,
+                        )
+                    }
+                }
+            }.launchIn(viewModelScope)
+    }
 
     fun onSearchTextChanged(searchText: TextFieldValue) {
         mutableState.update { currentState ->
